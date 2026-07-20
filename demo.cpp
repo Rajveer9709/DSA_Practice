a455701c92b5fc27c26deb2a5e5762e3792053ac
Rajveer9709 <rajveernandkar@gmail.com>
2026-07-20 19:14:22 +0530

    finding lowest/highest freq element

--- demo.cpp
+++ demo.cpp
@@ -20,13 +20,38 @@ int main()
         cout << it.first << "->" << it.second << endl;
     }
 
+    int max_freq = -1, max_element = -1;
+
+    for (auto it : mpp)
+    {
+        if (it.second > max_freq)
+        {
+            max_freq = it.second;
+            max_element = it.first;
+        }
+    }
+
+    int min_freq = INT_MAX, min_element = -1;
+
+    for (auto it : mpp)
+    {
+        if (it.second < min_freq)
+        {
+            min_freq = it.second;
+            min_element = it.first;
+        }
+    }
+
+    cout << "Max frequency element is :" << max_element << " " << "and it's freq is:" << max_freq << endl;
+    cout << "Min frequency element is :" << min_element << " " << "and it's freq is:" << min_freq << endl;
+
     int q;
     cout << "Enter the number of number you want to find:";
     cin >> q;
     while (q--)
     {
         int number;
-        cout << "Enter the number:";
+        cout << "Enter the number:" << endl;
         cin >> number;
         cout << mpp[number] << " ";
     }
