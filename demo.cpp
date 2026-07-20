e98c06727df82ca50020071d61dfe58b74a35c7d
Rajveer9709 <rajveernandkar@gmail.com>
2026-07-20 23:32:13 +0530

    Bubble sort

--- demo.cpp
+++ demo.cpp
@@ -1,21 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void selection_sort(int arr[], int n)
+void bubble_sort(int arr[], int n)
 {
-    for (int i = 0; i <= n - 2; i++)
+    for (int i = n - 1; i >= 0; i--)
     {
-        int min = i;
-        for (int j = i; j <= n - 1; j++)
+        int swap = 0;
+        for (int j = 0; j <= i - 1; j++)
         {
-            if (arr[j] < arr[min])
-            {   
-                min = j;
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swap = 1;
             }
         }
-        int temp = arr[min];
-        arr[min] = arr[i];
-        arr[i] = temp;
+        if (swap == 0)
+        {
+            break;
+        }
+        cout << "Runs\n";
     }
 }
 
@@ -30,10 +35,9 @@ int main()
     {
         cin >> arr[i];
     }
-    selection_sort(arr, n);
+    bubble_sort(arr, n);
     for (int i = 0; i < n; i++)
     {
-       cout << arr[i] << " ";
+        cout << arr[i] << " ";
     }
-
 }
